871c1d8244647ba929f2be8f1ca9de22e9cfc552
eesa <eesayousufyousuf@gmail.com>
2025-09-06 14:38:01 +0530

    dy

--- day01.py
+++ day01.py
@@ -1,2 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# Generate the pattern using parametric equations
+t = np.linspace(0, 10 * np.pi, 1000)
+x = np.sin(3 * t) * np.cos(2 * t)
+y = np.sin(2 * t) * np.cos(3 * t)
+
+
