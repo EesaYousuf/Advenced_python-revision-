4c7c3cced0d8c31d214e8d5a67588de2085a9536
eesa <eesayousufyousuf@gmail.com>
2025-09-06 14:39:17 +0530

    dy3

--- day01.py
+++ day01.py
@@ -5,4 +5,10 @@ t = np.linspace(0, 10 * np.pi, 1000)
 x = np.sin(3 * t) * np.cos(2 * t)
 y = np.sin(2 * t) * np.cos(3 * t)
 
+# Create the plot
+plt.figure(figsize=(6,6), facecolor='black')
+plt.plot(x, y, color='cyan', linewidth=2)
+plt.axis('off')  # Hide axes
 
+# Display the art
+plt.show()
