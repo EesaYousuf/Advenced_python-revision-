4cdb2f36e9c23585b853c185cb47f52c44b620ce
eesa <eesayousufyousuf@gmail.com>
2025-09-07 12:12:10 +0530

    try1

--- day02.py
+++ day02.py
@@ -3,4 +3,13 @@ nums = [1, 2, 3]
 it = iter(nums)
 print(next(it))  # 1
 print(next(it))  # 2
+# Generator Example
+def countdown(n):
+    while n > 0:
+        yield n
+        n -= 1
+
+for val in countdown(5):
+    print(val)
+
 
