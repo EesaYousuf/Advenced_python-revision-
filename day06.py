9e135c31fae7d82b55d44c13a580c4105944138a
eesa <eesayousufyousuf@gmail.com>
2025-09-12 11:54:58 +0530

    try2

--- day06.py
+++ day06.py
@@ -2,3 +2,9 @@ class Car:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
+        self.speed = 0 
+# Object creation
+car1 = Car("Toyota", "Corolla")
+print(car1.brand)   # Toyota
+print(car1.model)   # Corolla
+                        
