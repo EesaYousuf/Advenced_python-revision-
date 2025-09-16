9e8bce8bb354bc785747c7609ec0f95e755a665a
eesa <eesayousufyousuf@gmail.com>
2025-09-16 23:30:21 +0530

    try4

--- day07.py
+++ day07.py
@@ -3,10 +3,19 @@ from functools import reduce
 # Lambda for quick inline function
 add = lambda a, b: a + b
 print(add(3, 5))
-# Map
+# # Map
+# nums = [1, 2, 3, 4]
+# doubled = list(map(lambda x: x*2, nums))
+# # Reduce (cumulative)
+# product = reduce(lambda a, b: a*b, nums)
+
+# print(doubled, evens, product) # type: ignore
+from functools import reduce
+
 nums = [1, 2, 3, 4]
+
 doubled = list(map(lambda x: x*2, nums))
-# Reduce (cumulative)
+evens = list(filter(lambda x: x % 2 == 0, nums))  # <- must be here
 product = reduce(lambda a, b: a*b, nums)
 
 print(doubled, evens, product)
